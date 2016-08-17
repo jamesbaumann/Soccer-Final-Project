8b323427a13e1d04168aad31e62cae36e533b9b1
jamesbaumann19 <james_baumann@horacemann.org>
2016-08-17 17:49:07 +0000

    Updated now with our asking for zip codes added

--- application_controller.rb
+++ application_controller.rb
@@ -1,9 +1,21 @@
 require 'bundler'
 Bundler.require
-require_relative 'models/model.rb'
+require_relative 'models/model'
 
 class ApplicationController < Sinatra::Base
+
   get '/' do
     erb :index
   end
+
+  get '/soccer_result' do
+    erb :index
+  end
+
+  post '/soccer_result' do
+
+    @soccer_field = soccer_zips(params[:soccer_result])
+    erb :soccer_result
+  end
+
 end
---
+++ models/model.rb
@@ -0,0 +1,9 @@
+
+
+
+def soccer_zips(soccer_zip)
+  soccer_parks = {"10028"=>"Central Park"}
+  soccer_answer = soccer_parks[soccer_zip]
+end
+
+puts soccer_zips(10028)
